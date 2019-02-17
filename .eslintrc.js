8d0a7daa4cc0504b76ec8d0ec17c961dae81feca
Swann CASTEL <swann.castel@hotmail.fr>
2019-02-17 16:17:05 +0100

    init

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "env": {
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "prettier"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "rules": {
+        "prettier/prettier": "warn"
+    },
+    "plugins": ["prettier"]
+};

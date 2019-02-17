10b01add1e590c3518731bd6ec92ae943c90fa5f
Swann CASTEL <swann.castel@hotmail.fr>
2019-02-17 21:19:16 +0100

    refacto(db.js): remove useless ejs

--- generator/mock/template/mock/db.js
+++ generator/mock/template/mock/db.js
@@ -1,7 +1,9 @@
-<%_ if (options.addMockServer) { _%>
 module.exports = () => {
   return {
-    '<%= options.viewName %>': [{ id: 1, name: 'Paul' }]
+    '<%= options.viewName %>': [
+      { id: 1, name: 'Paul' },
+      { id: 2, name: 'Bob' },
+      { id: 3, name: 'Eric' }
+    ]
   }
 }
-<%_ } _%>

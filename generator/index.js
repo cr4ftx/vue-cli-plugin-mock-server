44aa9c018ee53ef71e3621a1d70d0664563ec137
Swann CASTEL <swann.castel@hotmail.fr>
2019-02-18 20:55:29 +0100

    refacto(template): move view creation in template dir

--- generator/index.js
+++ generator/index.js
@@ -1,48 +1,9 @@
-const fs = require('fs')
-const path = require('path')
-
-function toPascalCase(kebabCaseStr) {
-  return kebabCaseStr
-    .split('-')
-    .map(word => {
-      const [h, ...t] = word
-      return h.toUpperCase() + t.join('')
-    })
-    .join('')
-}
-
-const getView = name => `
-<template>
-  <div>
-    <h1>${name}</h1>
-  </div>
-</template>
-
-<script>
-export default {
-  name: '${name}'
-}
-</script>
-`.trim()
-
 module.exports = (api, options) => {
+  require('./template')(api, options)
+
   if (options.addMockServer) {
-    require('./mock')(api, options)
+    require('./mock')(api)
   }
 
-  api.onCreateComplete(() => {
-    try {
-      const viewDirPath = path.resolve('src/views', options.viewName)
-      const fileName = toPascalCase(options.viewName)
-      const dest = path.resolve(`${viewDirPath}/${fileName}.vue`)
-
-      fs.mkdirSync(viewDirPath)
-      fs.writeFileSync(dest, getView(options.viewName), 'utf8')
-
-      api.exitLog('Thanks for using effisoft plugin 👋')
-    } catch(err) {
-      api.exitLog(err.message, 'error')
-    }
-  })
-
+  api.exitLog('Thanks for using effisoft plugin 👋')
 }
